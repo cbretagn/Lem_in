e347912ecf84afc9391fd287bb31a2a3745ff8dd
Camille BRETAGNON <cbretagn@e3r1p6.42.fr>
2019-11-12 14:51:26 +0100

    utility

---
+++ lem_in.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 14:12:42 by cbretagn          #+#    #+#             */
+/*   Updated: 2019/11/12 14:40:34 by cbretagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# include "libft/libft.h"
+# include "get_next_line.h"
+# include <unistd.h>
+# include <stdlib.h>
+
+int			hash_fun(char *str);
+char		**put_in_table(char *str, char **tab, int size);
+int			search_in_table(char *str, char **tab, int size);
+
+#endif
