4629bf996bb6290b9c457703321de00c6310d176
Camille BRETAGNON <cbretagn@e3r1p6.42.fr>
2019-11-12 14:55:44 +0100

    hash table functions

---
+++ src/hash_table.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hash_table.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 14:11:49 by cbretagn          #+#    #+#             */
+/*   Updated: 2019/11/12 14:49:11 by cbretagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lem_in.h"
+
+int			hash_fun(char *str)
+{
+	char	c;
+	int		i;
+	int		hash;
+
+	hash = 456;
+	i = -1;
+	while ((c = str[++i]))
+		hash = hash * 31 + c;
+	return (hash);
+}
+
+char		**put_in_table(char *str, char **tab, int size)
+{
+	int		hash;
+	int		quadra;
+
+	quadra = 1;
+	hash = hash_fun(str) % size;
+	while (tab[hash] != NULL)
+	{
+		hash = (hash + quadra * quadra) % size;
+		quadra++;
+	}
+	if (!(tab[hash] = ft_strdup(str)))
+		return (NULL);
+	return (tab);
+}
+
+int			search_in_table(char *str, char **tab, int size)
+{
+	int		hash;
+	int		quadra;
+
+	quadra = 1;
+	hash = hash_fun(str) % size;
+	while (ft_strcmp(str, tab[hash]) != 0)
+	{
+		hash = (hash + quadra * quadra) % size;
+		quadra++;
+	}
+	return (hash);
+}
