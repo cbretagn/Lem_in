fcb4695e71e7e0452f7c86cf9ca2bea2a1a463b9
cbretagn <cbretagn@student.42.fr>
2019-11-12 15:50:41 +0100

    verif repetition in hash fun

--- src/hash_table.c
+++ src/hash_table.c
@@ -6,11 +6,22 @@
 /*   By: cbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 14:11:49 by cbretagn          #+#    #+#             */
-/*   Updated: 2019/11/12 14:49:11 by cbretagn         ###   ########.fr       */
+/*   Updated: 2019/11/12 15:50:17 by cbretagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lem_in.h"
+
+void		delete_table(char **tab, int size)
+{
+	int		i;
+
+	i = -1;
+	while (++i < size)
+		ft_strdel(&tab[i]);
+	free(tab);
+	tab = NULL;
+}
 
 int			hash_fun(char *str)
 {
@@ -34,11 +45,19 @@ char		**put_in_table(char *str, char **tab, int size)
 	hash = hash_fun(str) % size;
 	while (tab[hash] != NULL)
 	{
+		if (ft_strcmp(str, tab[hash]) == 0)
+		{
+			delete_table(tab, size);
+			return (NULL);
+		}
 		hash = (hash + quadra * quadra) % size;
 		quadra++;
 	}
 	if (!(tab[hash] = ft_strdup(str)))
+	{
+		delete_table(tab, size);
 		return (NULL);
+	}
 	return (tab);
 }
 
