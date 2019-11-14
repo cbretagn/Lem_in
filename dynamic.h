7d000e55f840d42d6fd141e1f0a4588f7c7ee46a
Sarah Dahan <sarahdahan3012@gmail.com>
2019-11-14 16:29:22 +0100

    proto changed in dynamic.h

--- dynamic.h
+++ dynamic.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dynamic.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sadahan <sadahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:00:58 by cbretagn          #+#    #+#             */
-/*   Updated: 2019/11/12 17:56:09 by cbretagn         ###   ########.fr       */
+/*   Updated: 2019/11/14 16:07:50 by sadahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ typedef struct		s_dstring
 
 t_dstring			*create_dstring(unsigned int cap, char *str);
 t_dstring			*push_str(t_dstring *dest, char *src);
-t_dstring			*push_str(t_dstring *dest, char *src, int n);
+t_dstring			*push_str_nchar(t_dstring *dest, char *src, int n);
 void				delete_dstring(t_dstring *del);
 
 #endif
