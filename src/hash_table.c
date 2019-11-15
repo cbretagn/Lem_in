5907ec6a4b8088201e997c07c1dbf552cbfaef0d
Sarah Dahan <sarahdahan3012@gmail.com>
2019-11-15 14:37:50 +0100

    bug fixed : add protection in search-table to protect handle-tubes when room doesn't exist

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hash_table.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sadahan <sadahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 14:11:49 by cbretagn          #+#    #+#             */
-/*   Updated: 2019/11/14 18:36:18 by cbretagn         ###   ########.fr       */
+/*   Updated: 2019/11/15 14:35:41 by sadahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,9 +61,16 @@ char		**put_in_table(char *str, char **tab, int size)
 int			search_in_table(char *str, char **tab, int size)
 {
 	int		hash;
+	int		i;
 
+	i = 0;
 	hash = hash_fun(str) % size;
-	while (ft_strcmp(str, tab[hash]) != 0)
+	while (ft_strcmp(str, tab[hash]) != 0 && i < size)
+	{
 		hash = (hash + 1) % size;
+		i++;
+	}
+	if (i == size)
+		return (-1);
 	return (hash);
 }
